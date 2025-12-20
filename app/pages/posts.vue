a75f75e65efad4d28ad968ea66459069dad3ca1f
liyang <272891021@qq.com>
2025-12-21 01:08:26 +0800

    重构帖子展示逻辑，按年份分组并优化模板结构

--- app/pages/posts.vue
+++ app/pages/posts.vue
@@ -10,33 +10,56 @@ const { data } = await useAsyncData(async () => {
   })
   return { tags, posts }
 })
+
+const postsByYear = computed(() => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const grouped = new Map<string, any[]>()
+  data.value?.posts.forEach((post) => {
+    const year = post.date.slice(0, 4)
+    grouped.set(year, [...(grouped.get(year) || []), post])
+  })
+  return grouped
+})
 </script>
 
 <template>
-  <div class="flex flex-col items-center gap-4 max-w-2xl mx-auto pt-8 md:pt-32">
-    <!-- <div class="flex gap-2 w-full">
-      <Badge
-        v-for="t in data.tags.keys()"
-        :key="t"
-        :label="t"
-      />
-    </div> -->
+  <div class="pt-8 md:pt-32">
     <div
-      v-for="p in data?.posts"
-      :key="p.hash"
-      class="flex justify-between gap-0 w-full hover:brightness-130 cursor-pointer"
+      v-for="y in postsByYear.keys()"
+      :key="y"
+      class="flex flex-col items-center gap-4 max-w-2xl mx-auto mb-16"
     >
-      <h1 class="text-sm md:text-xl">
-        {{ p.title }}
-      </h1>
-      <div class="hidden lg:flex gap-2 text-sm text-secondary-content">
-        <span v-if="p.tags"> # </span>
-        <span
-          v-for="tag in p.tags"
-          :key="tag"
-        > {{ tag }} </span>
+      <!-- <h1
+        class="text-8xl font-mono text-transparent absolute z-0"
+        :style="{
+          '-webkit-text-stroke': '4px oklch(30% 0.04 256.788)',
+          'left': '-2rem',
+        }"
+      >
+        {{ y }}
+      </h1> -->
+      <p class="text-secondary-content mr-auto flex gap-2">
+        <span>{{ y }}</span>
         <span> | </span>
-        <span class="font-mono">{{ p.date.slice(0, 10) }}</span>
+        <span>{{ `${postsByYear.get(y)?.length} post${postsByYear.get(y)?.length === 1 ? '' : 's'}` }}</span>
+      </p>
+      <div
+        v-for="p in postsByYear.get(y)"
+        :key="p.hash"
+        class="flex justify-between gap-0 w-full hover:brightness-130 cursor-pointer"
+      >
+        <h1 class="text-sm md:text-xl">
+          {{ p.title }}
+        </h1>
+        <div class="hidden lg:flex gap-2 text-sm text-secondary-content">
+          <span v-if="p.tags"> # </span>
+          <span
+            v-for="tag in p.tags"
+            :key="tag"
+          > {{ tag }} </span>
+          <span> | </span>
+          <span class="font-mono">{{ p.date.slice(0, 10) }}</span>
+        </div>
       </div>
     </div>
   </div>
