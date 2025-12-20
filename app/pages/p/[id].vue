24d15248c540a263a48b8f85cbf0cae339126bf4
liyang <272891021@qq.com>
2025-12-21 01:55:30 +0800

    Refactor code structure for improved readability and maintainability

--- app/pages/p/[id].vue
+++ app/pages/p/[id].vue
@@ -1,7 +1,16 @@
 <script lang="ts" setup>
 const route = useRoute()
+const { data } = await useAsyncData(async () => {
+  return await queryCollection('posts').where('hash', '=', route.params.id).first()
+})
 </script>
 
 <template>
-  <div>{{ route.params.id }}</div>
+  <div class="max-w-2xl mx-auto">
+    <ContentRenderer
+      v-if="data"
+      class="flex flex-col gap-4"
+      :value="data"
+    />
+  </div>
 </template>
