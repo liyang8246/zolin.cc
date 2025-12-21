659c3a7d45bc05d49af294003bee94ad55b68b8e
liyang <272891021@qq.com>
2025-12-21 16:22:21 +0800

    优化 CodeEditor 组件的主题设置，将高亮器和主题配置移入 onMounted 钩子中，并调整模板结构

--- app/components/CodeEditor.vue
+++ app/components/CodeEditor.vue
@@ -17,22 +17,19 @@ const {
 
 const container = ref<HTMLElement>()
 let editorView: EditorView | null = null
-const highlighter = await useHighlighter(language)
-
-const transTheme = EditorView.theme({
-  '.cm-gutters': {
-    backgroundColor: 'oklch(13% 0.028 261.692)',
-  },
-  '.cm-activeLineGutter': {
-    backgroundColor: 'oklch(13% 0.028 261.692)',
-  },
-  '.cm-content': {
-    backgroundColor: 'oklch(13% 0.028 261.692)',
-  },
-})
 
 onMounted(async () => {
   if (!container.value) return
+
+  const highlighter = await useHighlighter(language)
+  const transTheme = EditorView.theme({
+    '.cm-gutters': {
+      backgroundColor: 'oklch(13% 0.028 261.692)',
+    },
+    '.cm-gutters-before': {
+      borderColor: 'oklch(27.8% 0.033 256.848)',
+    },
+  })
 
   const state = EditorState.create({
     doc: model.value.trimEnd(),
@@ -73,5 +70,20 @@ watch(model, (newValue) => {
 </script>
 
 <template>
-  <div ref="container" />
+  <div class="relative">
+    <div ref="container" />
+  </div>
 </template>
+
+<style>
+.cm-scroller::-webkit-scrollbar {
+  height: 8px;
+}
+.cm-scroller::-webkit-scrollbar-thumb {
+  background-color: oklch(27.8% 0.033 256.848);
+  border-radius: calc(infinity * 1px);
+}
+.cm-scroller::-webkit-scrollbar-thumb:hover {
+  background-color: oklch(37.3% 0.034 259.733);
+}
+</style>
