9f125a5a9fa39e910a2054f3fa47a58b5c400f36
liyang <272891021@qq.com>
2025-12-21 12:02:47 +0800

    重构 app.vue 组件，移除冗余代码并优化布局结构

--- app/app.vue
+++ app/app.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
-const { data: page } = await useAsyncData(() => {
-  return queryCollection('posts').where('__hash__', '=', 123).all()
-})
-page.value?.forEach((p) => {
-  console.log(p)
-})
 </script>
 
 <template>
-  <div class="bg-linear-to-b from-slate-900 from-[128px] to-primary to-[768px] min-h-screen text-primary-content p-4 md:p-8 font-default">
-    <NavBar />
-    <NuxtPage />
+  <div class="bg-primary">
+    <div class="bg-linear-to-b from-slate-900 from-[128px] to-transparent to-[768px] min-h-screen text-primary-content p-4 md:p-8 font-default relative">
+      <FlickeringGrid />
+      <NavBar />
+      <NuxtPage />
+    </div>
   </div>
 </template>
