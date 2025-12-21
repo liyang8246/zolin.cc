34bcf32920a73f731f0a5e16e60f64e3e61aeedb
liyang <272891021@qq.com>
2025-12-22 00:47:39 +0800

    chore: add Vercel configuration file for deployment

--- app/app.vue
+++ app/app.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div class="bg-primary">
-    <div class="flex flex-col bg-linear-to-b from-slate-900 from-[128px] to-transparent to-[768px] min-h-screen text-primary-content p-4 md:p-8 font-default">
+    <div class="bg-linear-to-b from-slate-900 from-[128px] to-transparent to-[768px] min-h-screen text-primary-content p-4 md:p-8 font-default">
       <NavBar />
       <NuxtPage />
       <div class="text-sm text-gray-500 mt-auto">
