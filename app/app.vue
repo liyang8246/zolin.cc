f85aa61c5edea3a474665f255fae53e522d18b06
liyang <272891021@qq.com>
2025-12-22 23:47:22 +0800

    重构 friends 页面以显示友链列表，添加 favicon、名称和加入日期信息；更新样式以提升用户体验

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { Analytics } from '@vercel/analytics/nuxt'
-import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+import { SpeedInsights } from '@vercel/speed-insights/nuxt'
 </script>
 
 <template>
--- app/pages/friends.vue
+++ app/pages/friends.vue
@@ -1,7 +1,40 @@
 <script lang="ts" setup>
-
+const friends = [
+  { favicon: 'https://blog.halfsweet.cn/img/Headshot.jpg', name: 'HalfSweet\'s Blog', url: 'https://blog.halfsweet.cn', on: '2025-12-22' },
+  { favicon: 'https://jelasin.github.io/images/touxiang.jpg', name: 'Jelasin\'s Blog', url: 'https://jelasin.github.io', on: '2025-12-22' },
+]
 </script>
 
 <template>
-  <div>coming soon</div>
+  <div class="pt-8 md:pt-32">
+    <div class="flex flex-col gap-4 max-w-2xl mx-auto">
+      <NuxtLink
+        v-for="f in friends"
+        :key="f.url"
+        target="_blank"
+        :to="f.url"
+        class="w-full flex items-center gap-4 hover:brightness-130 cursor-pointer"
+      >
+        <NuxtImg
+          :src="f.favicon"
+          class="size-8 rounded"
+        />
+        <div class="text-xl">{{ f.name }}</div>
+        <div class="mx-auto" />
+        <div class="flex gap-2 text-sm text-secondary-content">
+          <span>Joined on</span>
+          <span class="font-mono">{{ f.on }}</span>
+        </div>
+      </NuxtLink>
+      <p class="text-secondary-content text-sm mt-4">
+        欢迎大家
+        <a
+          href="https://github.com/liyang8246/zolin.cc/issues/new"
+          class="underline"
+          target="_blank"
+        >留言</a>
+        添加自己的友链 :)
+      </p>
+    </div>
+  </div>
 </template>
