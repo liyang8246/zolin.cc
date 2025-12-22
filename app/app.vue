f472251d54fa27d44aca8039c5c910ad1defe12f
liyang <272891021@qq.com>
2025-12-22 16:57:23 +0800

    添加 Vercel 分析工具以增强网站分析功能

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
 </script>
 
 <template>
@@ -11,6 +12,7 @@
       </div>
     </div>
   </div>
+  <Analytics />
 </template>
 
 <style>
